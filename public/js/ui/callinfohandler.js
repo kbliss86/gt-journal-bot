79151990f35e4c1de6c988d0b71e16701f3e0d7d
Kendall Bliss <kendall.bliss@grapetree.com>
2024-02-21 15:32:28 -0600

    added comments and cleaned the current code

--- public/js/ui/callinfohandler.js
+++ public/js/ui/callinfohandler.js
@@ -1,9 +1,12 @@
+//Function for handling HCP ID input and fetching HCP info
 function fetchHCPInfo() {
+    //Get HCP ID from input
     const hcpID = document.getElementById('hcpID').value;
     if (!hcpID) {
         return;
     }
 
+    //Fetch HCP info from server
     fetch(`/api/temps?tempIdIn=${hcpID}`)
         .then(response => {
             // console.log(response.json());
@@ -15,26 +18,23 @@ function fetchHCPInfo() {
         .then(data => {
             // const hcpInfoDiv = document.getElementById('hcpInfo');
             const tempRecord = data[0];
-
+            //Populate HCP info in output div(s)
             document.getElementById('hcpIDOutput').textContent = tempRecord.tempId;
             document.getElementById('hcpNameOutput').textContent = tempRecord.firstName;
             document.getElementById('hcpCityOutput').textContent = tempRecord.city;
             document.getElementById('hcpCertOutput').textContent = tempRecord.certification;
-            // hcpInfoDiv.innerHTML = `
-            //     <p>HCP ID: ${tempRecord.tempId}</p>
-            //     <p>First Name: ${tempRecord.firstName}</p>
-            //     <p>Last Name: ${tempRecord.lastName}</p>
-            //     <p>Address: ${tempRecord.address}</p>
-            //     <!-- Add more fields as needed -->
-            // `;
-            // console.log(hcpInfoDiv)
+
+            //Store HCP ID in sessionStorage for use in other functions
+            sessionStorage.setItem('hcpID', tempRecord.tempId);
         })
         .catch(error => console.error('Error:', error));
 }
 
+//Function for handling HCP ID button click
 function hcpIdButtonClick(event) {
     event.preventDefault();
     fetchHCPInfo();
 }
 
+//Add event listener to HCP ID button
 document.getElementById('hcpIdSubmit').addEventListener('click', hcpIdButtonClick);
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Import
 const express = require("express");
 const http = require("http");
 const { createClient } = require("@deepgram/sdk");
@@ -5,20 +6,23 @@ const dotenv = require("dotenv");
 const routes = require("./server/index");
 dotenv.config();
 
+//Environment Variables
 const client = createClient(process.env.DEEPGRAM_API_KEY);
 const clearConnect = createClient(process.env.CLEAR_CONNECT_API_KEY);
 
+//Server
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app); //Not Used
 
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static("public/"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+//Why is this needed?
 const getProjectId = async () => {
   const { result, error } = await client.manage.getProjects();
 
@@ -29,6 +33,7 @@ const getProjectId = async () => {
   return result.projects[0].project_id;
 };
 
+//Pull Deepgram API Key from Deepgram
 const getTempApiKey = async (projectId) => {
   const { result, error } = await client.manage.createProjectKey(projectId, {
     comment: "short lived",
@@ -43,6 +48,7 @@ const getTempApiKey = async (projectId) => {
   return result;
 };
 
+//Route to Pull Deepgram API Key into other site elements
 app.get("/key", async (req, res) => {
   const projectId = await getProjectId();
   const key = await getTempApiKey(projectId);
@@ -50,12 +56,15 @@ app.get("/key", async (req, res) => {
   res.json(key);
 });
 
+//Route to Pull ClearConnect API Key into other site elements
 app.get("/ccKey", async (req, res) => {
   res.json({ key: process.env.CLEAR_CONNECT_API_KEY });
 });
 
+//Middleware for connecting server.js to routes
 app.use(routes);
 
+//Test Route to Catch Failures in Express Routes
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
